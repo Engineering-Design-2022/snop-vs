233efe3fc0b437193472a99d0e8095879e8d2cff
yuto_ka <31912@toyota.kosen-ac.jp>
2022-09-17 11:13:21 +0900

    テーブル作成　ダミーデータ入力

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -5,3 +5,27 @@ GRANT ALL PRIVILEGES ON DATABASE db_dev TO dev;
 
 DROP TABLE IF EXISTS number_of_people;
 DROP TABLE IF EXISTS room;
+
+CREATE TABLE room(
+    id serial primary key,
+    name varchar(30) not null,
+    description text
+);
+
+CREATE TABLE number_of_people(
+    id serial primary key,
+    number integer not null,
+    created_at timestamp with time zone not null default CURRENT_TIMESTAMP,
+    room_id int not null,
+    foreign key (room id) references room(id)
+);
+
+insert into room (name, description) values ('311講義室','1階、4I教室');
+insert into room (name, description) values ('312講義室','1階、5I教室');
+insert into room (name, description) values ('情報基礎実験室','4階');
+insert into room (name, description) values ('情報回路実験室','4階');
+
+insert into number_of_people (number, room_id) values (7, 1);
+insert into number_of_people (number, room_id) values (3, 2);
+insert into number_of_people (number, room_id) values (10, 3);
+insert into number_of_people (number, room_id) values (0, 4);
